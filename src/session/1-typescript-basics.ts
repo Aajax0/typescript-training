e72261d1b457d367d22d965c0f799daa1269b344
theRealRobG <robertnw5@gmail.com>
2016-10-20 21:30:32 +0100

    jam

--- src/session/1-typescript-basics.ts
+++ src/session/1-typescript-basics.ts
@@ -1,17 +1,17 @@
 namespace TypeScriptDemo {
 
     function isNameOfGivenLength(name: string, length: number): boolean {
-        //return name.length === length;
+        return name.length === length;
     }
 
     function generateRandomScoreForName(name: string): [string, number] {
-        // let score = Math.random() * 5;
-        // return [name, score];
+        let score = Math.random() * 5;
+        return [name, score];
     }
 
     type RandomScore = [string, number];
     function generateScoresForNames(...names: Array<string>): Array<RandomScore> {
-        // return names.map(name => generateRandomScoreForName(name));
+        return names.map(name => generateRandomScoreForName(name));
     }
 
     type NameApiResponse = {
@@ -23,18 +23,18 @@ namespace TypeScriptDemo {
     };
 
     function getPerson(): Promise<NameApiResponse> {
-        // const response = {
-        //     name: 'Rob',
-        //     score: 5,
-        //     languages: {
-        //         list: ['TypeScript']
-        //     }
-        // }
-        // return new Promise<NameApiResponse>((resolve, reject) => {
-        //     setTimeout(() => {
-        //         resolve(response);
-        //     }, 1000);
-        // });
+        const response = {
+            name: 'Rob',
+            score: 5,
+            languages: {
+                list: ['TypeScript']
+            }
+        }
+        return new Promise<NameApiResponse>((resolve, reject) => {
+            setTimeout(() => {
+                resolve(response);
+            }, 1000);
+        });
     }
 
     export function executeSession1() {

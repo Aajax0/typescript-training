639173f5a2c1cb86bbc2eba5feb9b74f8269a7fa
theRealRobG <robertnw5@gmail.com>
2016-10-20 21:32:20 +0100

    more jam

--- src/session/1-typescript-basics.ts
+++ src/session/1-typescript-basics.ts
@@ -1,42 +1,4 @@
 namespace TypeScriptDemo {
-
-    function isNameOfGivenLength(name: string, length: number): boolean {
-        //return name.length === length;
-    }
-
-    function generateRandomScoreForName(name: string): [string, number] {
-        // let score = Math.random() * 5;
-        // return [name, score];
-    }
-
-    type RandomScore = [string, number];
-    function generateScoresForNames(...names: Array<string>): Array<RandomScore> {
-        // return names.map(name => generateRandomScoreForName(name));
-    }
-
-    type NameApiResponse = {
-        name: string,
-        score: number,
-        languages: {
-            list: Array<string>
-        }
-    };
-
-    function getPerson(): Promise<NameApiResponse> {
-        // const response = {
-        //     name: 'Rob',
-        //     score: 5,
-        //     languages: {
-        //         list: ['TypeScript']
-        //     }
-        // }
-        // return new Promise<NameApiResponse>((resolve, reject) => {
-        //     setTimeout(() => {
-        //         resolve(response);
-        //     }, 1000);
-        // });
-    }
-
     export function executeSession1() {
         console.warn('executing typescript basics session');
     };
--- src/session/2-oop-in-typescript.ts
+++ src/session/2-oop-in-typescript.ts
@@ -1,30 +1,4 @@
 namespace TypeScriptDemo {
-
-    interface ILabelService {
-        getApplyButtonText(): string;
-        getDaysUntilExpiryMessage(days: number): string;
-        getDescriptionForFilm(crid: string): Promise<string>;
-    }
-    class EnglishLabelService implements ILabelService {
-        // public getApplyButtonText(): string {
-        //     return 'Apply Now';
-        // }
-        // public getDaysUntilExpiryMessage(days: number): string {
-        //     const plural = days > 1 ? 's' : '';
-        //     return `This expires in ${days} day${plural}`;
-        // }
-        // public getDescriptionForFilm(crid: string): Promise<string> {
-        //     return new Promise<string>((resolve, reject) => {
-        //         setTimeout(() => {
-        //             return 'Truly a great film!';
-        //         });
-        //     });
-        // }
-    }
-    class GermanLabelService implements ILabelService {
-
-    }
-
     export function executeSession2() {
         console.warn('executing oop in typescript session');
     };  
